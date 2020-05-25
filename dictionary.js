b0936657cde44765ecb7d2532c90fa2a71680b8a
Travis <tbyrman@hotmail.com>
2020-05-25 16:11:48 +1200

    Adding photos

---
+++ dictionary.js
@@ -0,0 +1,11 @@
+var family = ["Alex", "Henry", "John", "Bob", "Christine"];
+
+var person = {
+  firstName: "Alex",
+  lastName: "Jungo",
+  age: 27,
+  dateOfBirth: "16th July",
+  family: family,
+};
+
+console.log(person);
--- hashTable.js
+++ hashTable.js
@@ -1,3 +1,4 @@
+/*
 function Hash() {
   this.length = 0;
   this.items = new Array();
@@ -6,9 +7,7 @@ function Hash() {
       this.items[arguments[i]] = arguments[i + 1];
       this.length++;
     }
-  }
-
-  this.removeItem = function (in_key) {
+  }  this.removeItem = function (in_key) {
     var tmp_value;
     if (typeof this.items[in_key] != "undefined") {
       this.length--;
@@ -21,7 +20,6 @@ function Hash() {
   this.getItem = function (in_key) {
     return this.items[in_key];
   };
-
   this.setItem = function (in_key, in_value) {
     if (typeof in_value != "undefined") {
       if (typeof this.items[in_key] == "undefined") {
@@ -40,7 +38,7 @@ function Hash() {
 
 var myHash = new Hash("1", [1, 10, 5], "2", [2, 1, 8], "3", [3, 30, 300]);
 
-//********************************************************************************** */
+
 
 for (var i in myHash.items) {
   console.log("| KEY: " + i + " |, VALUE: " + myHash.items[i]);
@@ -53,3 +51,22 @@ console.log(myHash.removeItem(1));
 console.log(myHash.items);
 
 //********************************************************************************** */
+
+/*
+
+
+function Hash() {
+    length = 0 (bucket length)
+    items = a new array
+    for (i is less than length of arguments (bucket length); i + 2) {
+      if (typeof arguments[i + 1] (index value) is not hashed) {
+        index of items array = arguments index;
+        length ++ (increase bucket size)
+      }
+    }
+}
+  
+  var myHash = new Hash("index value", data);
+  
+
+  */
